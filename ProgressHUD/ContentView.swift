8f8df0bc09ca6012e274b588d4def123dd09bca0
Shogo Takeuchi <56812464+ShogoTakeuchi@users.noreply.github.com>
2021-10-18 14:34:00 +0900

    Initial Commit

---
+++ ProgressHUD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProgressHUD
+//
+//  Created by Shogo Takeuchi on 2021/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProgressHUD/ProgressHUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressHUDApp.swift
+//  ProgressHUD
+//
+//  Created by Shogo Takeuchi on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct ProgressHUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
