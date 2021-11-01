49f18468e5a71f59c78d1c106fc84e70c8808e90
Shogo Takeuchi <56812464+ShogoTakeuchi@users.noreply.github.com>
2021-11-01 09:37:21 +0900

    fix: package name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "progress-hud",
+            name: "ProgressHud",
             targets: ["progress-hud"]),
     ],
     dependencies: [
