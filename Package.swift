7a8279cbf5e2c21f0baf9e398c44d5c1b91cacd3
Shogo Takeuchi <56812464+ShogoTakeuchi@users.noreply.github.com>
2021-11-01 09:38:26 +0900

    fix: package name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ProgressHud",
+    name: "ProgressHUD",
     platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ProgressHud",
+            name: "ProgressHUD",
             targets: ["progress-hud"]),
     ],
     dependencies: [
